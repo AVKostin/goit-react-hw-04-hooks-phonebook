cf29f17b732b005d3f9c17d464c78bbb0c96f451
AVKostin <alexander.kostin.ua@gmail.com>
2022-05-22 18:34:05 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,65 +1,56 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import useLocalStorage from 'Hooks/useLocalStorage';
 import styles from './styles.module.css';
 import { FaJournalWhills } from 'react-icons/fa';
 import Form from '../Form';
 import Contacts from '../Contacts';
 import Filter from '../Filter';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '+38 095 459 1256' },
-      { id: 'id-2', name: 'Hermione Kline', number: '+38 095 443 8912' },
-      { id: 'id-3', name: 'Eden Clements', number: '+38 095 645 1779' },
-      { id: 'id-4', name: 'Annie Copeland', number: '+38 095 227 9126' },
-    ],
-    filter: '',
+export default function App(){
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '+38 095 459 1256' },
+    { id: 'id-2', name: 'Hermione Kline', number: '+38 095 443 8912' },
+    { id: 'id-3', name: 'Eden Clements', number: '+38 095 645 1779' },
+    { id: 'id-4', name: 'Annie Copeland', number: '+38 095 227 9126' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const addContact = contact => {
+    setContacts([...contacts, contact]);
   };
 
-
-  toAddContact = contact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+  const onFilterInput = value => {
+  setFilter(value);
   };
 
-  onFilterInput = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  onFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  const filteredContacts = () => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLocaleLowerCase())
     );
   };
 
-  onDeleteContact = e => {
-    const elemToRemove = e.currentTarget.parentNode.id;
-    this.setState({
-      contacts: this.state.contacts.filter(item => item.id !== elemToRemove),
-    });
+  const deleteContact = e => {
+  const elemToRemove = e.currentTarget.parentNode.id;
+  setContacts(contacts.filter(item => item.id !== elemToRemove));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
     return (
       <div className={styles.wrapper}>
         <h1 className={styles.h1}>
-           <FaJournalWhills size={28} className={styles.icon} />Phonebook
+        <FaJournalWhills size={28} className={styles.icon} />Phonebook
         </h1>
-        <Form addContact={this.toAddContact} contacts={contacts} />
+        <Form addContact={addContact} contacts={contacts} />
         <h2>Contacts</h2>
-        <Filter onInput={this.onFilterInput} />
+        <Filter onFilterInput={onFilterInput} />
         <Contacts
           contacts={contacts}
           filter={filter}
-          filteredContacts={this.onFilteredContacts}
-          deleteContact={this.onDeleteContact}
+          filteredContacts={filteredContacts}
+          deleteContact={deleteContact}
         />
+        {contacts.length === 0 && (
+        <p style={{ color: 'blue', textDecoration: 'underline' }}> no contacts available </p>
+      )}
       </div>
     );
-  }
-}
-
-export default App;
+};
